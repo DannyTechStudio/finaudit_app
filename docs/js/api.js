b32033557e83c94298836b942887453af65f6ef7
Daniel Offei <archimedes301@gmail.com>
2026-08-23 21:09:50 +0000

    chore(api): change API URL path

--- docs/js/api.js
+++ docs/js/api.js
@@ -1,7 +1,8 @@
 /*-----------------------------------------------------
         API BASE URL
 -----------------------------------------------------*/
-const API_BASE_URL = "http://localhost:8000/api";
+// const API_BASE_URL = "http://localhost:8000/api";
+const API_BASE_URL = "http://127.0.0.1:8000/api";
 
 
 //  Obtaining Token
